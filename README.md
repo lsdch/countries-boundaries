18389d9f3ef928be5d40380ad44bfba50a5e89a3
Louis Duchemin <louis.duchemin@univ-lyon1.fr>
2025-03-13 17:04:34 +0100

    fix dataset processing:
    - missing Antarctica metadata
    - merge geometries using ISO-3166 country code
    - use ISO-3166 country name and additional region metadata

--- README.md
+++ README.md
@@ -6,9 +6,16 @@ Original dataset was retrieved from [Marine Regions](https://www.marineregions.o
 transformed from Shapefile to GeoJSON,
 and then filtered to remove ambiguous areas, i.e. "Joint regime" and "Overlapping claim".
 
+Territories were merged together using property `ISO_TER1` (territory code according to ISO-3166)
+or `ISO_SOV1` (sovereign state code according to ISO-3166) when missing.
+
+Resulting dataset was enriched with ISO-3166 country metadata,
+using a curated table from [lukes/ISO-3166-Countries-with-Regional-Codes](https://github.com/lukes/ISO-3166-Countries-with-Regional-Codes/blob/master/all/all.csv) GitHub repository.
+
 ## Usage
 
 ```sh
+uv sync
 python scripts/processing.py
 ```
 
--- scripts/processing.py
+++ scripts/processing.py
@@ -1,24 +1,36 @@
-import json
+#%%
+import geopandas
+import pandas as pd
 
-def load_json(path: str) -> dict:
-    print(f"Loading {path}")
-    with open(path, "r") as f:
-        return json.load(f)
+features = geopandas.read_file("data/EEZ_land_union_v4_202410.json")
+
+# Fix missing Antarctica metadata from the dataset,
+# although it is defined in the ISO 3166 standard
+features.loc[features['UNION'] == 'Antarctica', 'ISO_SOV1'] = 'ATA'
+features.loc[features['UNION'] == 'Antarctica', 'SOVEREIGN1'] = 'Antarctica'
+
+# Use Territory code if available, otherwise use Sovereign code
+features['CODE'] = features['ISO_TER1'].replace('', None).fillna(features['ISO_SOV1'])
+
+# Filter out territories that have ambiguous status: Joint regime or Overlapping claim)
+filtered = features[features['POL_TYPE'].isin(['Joint regime (EEZ)', 'Overlapping claim']).eq(False)]
+
+dissolved = filtered[['CODE', 'geometry']].dissolve(by=['CODE'])
+
+print("Merged country boundaries:")
+print(dissolved)
 
 
-def main():
-    geojson_data = load_json("data/EEZ_land_union_v4_202410.json")
-    # country_codes = load_json("data/countries-codes.json")
-    with open("output/countries.json", "w") as f:
-        print("Filtering data")
-        filtered_features = [
-            feature for feature in geojson_data['features']
-            if feature['properties']['POL_TYPE']
-            in ['Landlocked country', 'Union EEZ and country']
-        ]
-        print("Writing data to: ./output/countries.json")
-        f.write(json.dumps(filtered_features) + "\n")
+country_codes = pd.read_csv("data/country_codes.csv")[['name', 'alpha-2', 'alpha-3', 'country-code', 'sub-region']]
+
+print("Processed dataset with ISO metadata:")
+merged = country_codes.merge(dissolved, left_on='alpha-3', right_on='CODE', how='left')
+print(merged)
+
+result = geopandas.GeoDataFrame(merged)
+result.plot()
+result.to_file("output/countries.json", driver="GeoJSON")
 
 
-if __name__ == "__main__":
-    main()
+
+# %%
